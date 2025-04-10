b9183ea10abce8329f9292bfb1368dfa765e6fac
Christian brito <114140097+ARMILANCER@users.noreply.github.com>
2025-04-10 12:35:47 +0200

    Delete webapp directory

--- webapp/listener.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('keydown', (e) => {
-    let movement =10;
-    if (e.key === 'ArrowUp') player.y -= player.speed+movement;
-    if (e.key === 'ArrowDown') player.y += player.speed+movement;
-    if (e.key === 'ArrowLeft') player.x -= player.speed+movement;
-    if (e.key === 'ArrowRight') player.x += player.speed+movement;
-    requestAnimationFrame(reDraw);
-  });
